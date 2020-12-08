dcd1c5f97a8b1c668910ee3464c9208ada054ffa
Shane <ignignokterr@gmail.com>
2020-12-08 14:28:00 -0800

    Fix character movement

--- src/systems/character.rs
+++ src/systems/character.rs
@@ -1,6 +1,8 @@
 //! Character systems
 
 use bevy::prelude::*;
+use bevy_rapier2d::physics::RigidBodyHandleComponent;
+use bevy_rapier2d::rapier::dynamics::RigidBodySet;
 
 use crate::components::character::*;
 use crate::resources::world::*;
@@ -11,8 +13,9 @@ use crate::resources::world::*;
 pub fn character_input_2d_keyboard_system(
     time: Res<Time>,
     world_bounds: Res<WorldBounds2D>,
+    mut rigidbodies: ResMut<RigidBodySet>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Character, &Sprite, &mut Transform)>,
+    mut query: Query<(&Character, &Sprite, &RigidBodyHandleComponent)>,
 ) {
     let mut direction = Vec2::default();
     if keyboard_input.pressed(KeyCode::Right) {
@@ -23,12 +26,18 @@ pub fn character_input_2d_keyboard_system(
         *direction.x_mut() -= 1.0;
     }
 
-    for (character, sprite, mut transform) in query.iter_mut() {
-        let half_width = sprite.size.x() / 2.0;
+    for (character, sprite, rbhandle) in query.iter_mut() {
+        if let Some(rigidbody) = rigidbodies.get_mut(rbhandle.handle()) {
+            let half_width = sprite.size.x() / 2.0;
 
-        let x = (transform.translation.x() + time.delta_seconds * direction.x() * character.speed)
-            .min(world_bounds.max.x() - half_width)
-            .max(world_bounds.min.x() + half_width);
-        transform.translation.set_x(x);
+            let mut position = rigidbody.position().clone();
+
+            let x = (position.translation.x + time.delta_seconds * direction.x() * character.speed)
+                .min(world_bounds.max.x() - half_width)
+                .max(world_bounds.min.x() + half_width);
+            position.translation.x = x;
+
+            rigidbody.set_next_kinematic_position(position)
+        }
     }
 }
